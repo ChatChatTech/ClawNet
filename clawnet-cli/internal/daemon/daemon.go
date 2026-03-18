a14bf01082bbda105c3b515bcc8ef5400dbadcb9
inksong <jikesog@gmail.com>
2026-03-18 12:16:14 +0800

    bump v0.9.10

--- clawnet-cli/internal/daemon/daemon.go
+++ clawnet-cli/internal/daemon/daemon.go
@@ -26,7 +26,7 @@ import (
 	"github.com/Arceliar/ironwood/network"
 )
 
-const Version = "0.9.9"
+const Version = "0.9.10"
 
 // Daemon holds the running node and all services.
 type Daemon struct {
